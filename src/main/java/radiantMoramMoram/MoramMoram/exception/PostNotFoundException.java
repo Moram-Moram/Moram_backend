280d76218bdd7921d70dbfe680809c58c11e53d8
yujina <jina6962@gmail.com>
2021-06-08 22:28:08 +0900

    [INIT]

---
+++ src/main/java/radiantMoramMoram/MoramMoram/exception/PostNotFoundException.java
@@ -0,0 +1,12 @@
+package radiantMoramMoram.MoramMoram.exception;
+
+import radiantMoramMoram.MoramMoram.error.BasicException;
+import radiantMoramMoram.MoramMoram.error.ErrorCode;
+
+public class PostNotFoundException extends BasicException {
+
+    public PostNotFoundException() {
+        super(ErrorCode.POST_NOT_FOUND);
+    }
+
+}
