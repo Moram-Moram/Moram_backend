67148ccd4fcfc109caa51f98207919871a728838
joungeun <lje0818ovo@naver.com>
2021-06-21 22:02:35 +0900

    [CORRECT] Change password type

--- src/main/java/radiantMoramMoram/MoramMoram/domain/user/User.java
+++ src/main/java/radiantMoramMoram/MoramMoram/domain/user/User.java
@@ -1,8 +1,11 @@
 package radiantMoramMoram.MoramMoram.domain.user;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Getter
 @Table(name = "tbl_user")
@@ -10,18 +13,32 @@ import javax.persistence.*;
 public class User {
     @Id
     private String id;
-    // private String password;
-    @Embedded
-    private Password password;
+    private String password;
     private String name;
-
-    public User(String id, Password pw, String name){
-        this.id = id;
-        this.password = pw;
-        this.name = name;
-    }
 
     public User() {
 
+    }
+
+    User(String id, String password, String name) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+    }
+
+    static public String pwEncrypt(String pw){
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(pw.getBytes());
+            StringBuilder bilder = new StringBuilder();
+            for(byte b: md.digest()){
+                bilder.append(String.format("%02x",b));
+            }
+            return bilder.toString();
+        } catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+            System.out.println("Password encryption failed");
+        }
+        return "";
     }
 }
