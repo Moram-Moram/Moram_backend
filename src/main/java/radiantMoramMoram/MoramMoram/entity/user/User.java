6219b47789efe785c1ce61f90835d57d5c19f226
joungeun <lje0818ovo@naver.com>
2021-07-07 22:32:56 +0900

    [ADD] add user entity column

--- src/main/java/radiantMoramMoram/MoramMoram/entity/user/User.java
+++ src/main/java/radiantMoramMoram/MoramMoram/entity/user/User.java
@@ -1,12 +1,19 @@
-package radiantMoramMoram.MoramMoram.domain.user;
+package radiantMoramMoram.MoramMoram.entity.user;
 
 import lombok.Builder;
 import lombok.Getter;
+import radiantMoramMoram.MoramMoram.entity.role.Role;
+import radiantMoramMoram.MoramMoram.security.auth.Authority;
+import radiantMoramMoram.MoramMoram.security.auth.AuthorityAttributeConverter;
 
 import javax.persistence.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
+// 루트 엔티티 (애그리거트)
 @Getter
 @Table(name = "tbl_user")
 @Entity
@@ -15,15 +22,22 @@ public class User {
     private String id;
     private String password;
     private String name;
+    private boolean whiteCheck;
+    @Enumerated(EnumType.ORDINAL)
+    private Authority role = Authority.ROLE_WATER_DROP;
+
+    @OneToMany(cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Role> roles = new ArrayList<>();
 
     public User() {
 
     }
 
-    User(String id, String password, String name) {
+    User(String id, String password, String name, boolean whiteCheck) {
         this.id = id;
         this.password = password;
         this.name = name;
+        this.whiteCheck = whiteCheck;
     }
 
     static public String pwEncrypt(String pw){
--- src/test/java/radiantMoramMoram/MoramMoram/UserTests.java
+++ src/test/java/radiantMoramMoram/MoramMoram/UserTests.java
@@ -3,10 +3,14 @@ package radiantMoramMoram.MoramMoram;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import radiantMoramMoram.MoramMoram.application.user.UserService;
-import radiantMoramMoram.MoramMoram.domain.user.User;
-import radiantMoramMoram.MoramMoram.application.user.UserDTO;
-import radiantMoramMoram.MoramMoram.domain.user.UserBuilder;
+import radiantMoramMoram.MoramMoram.entity.role.Role;
+import radiantMoramMoram.MoramMoram.entity.role.RoleID;
+import radiantMoramMoram.MoramMoram.repository.RoleRepository;
+import radiantMoramMoram.MoramMoram.security.auth.Authority;
+import radiantMoramMoram.MoramMoram.service.user.UserService;
+import radiantMoramMoram.MoramMoram.entity.user.User;
+import radiantMoramMoram.MoramMoram.payload.request.user.LoginRequest;
+import radiantMoramMoram.MoramMoram.entity.user.UserBuilder;
 
 
 @SpringBootTest(properties = "classpath:/application.yml")
@@ -14,25 +18,21 @@ public class UserTests {
     @Autowired
     UserService userService;
 
+
     @Test
     public void joinTest(){
-        String id = "whddd111";
+        String id = "whddd1110";
         String pw = "djssl";
         String name  = "이종은";
+        boolean whiteCheck = true;
 
         User user = new UserBuilder()
                 .setId(id)
                 .setPassword(pw)
                 .setName(name)
+                .setWhiteCheck(whiteCheck)
                 .build();
-        userService.join(user);
-    }
 
-    @Test
-    public void loginTest(){
-        String id = "whddms";
-        String pw = "djsdl";
-        UserDTO user = new UserDTO(id,pw);
-        userService.login(user);
+        userService.join(user);
     }
 }
