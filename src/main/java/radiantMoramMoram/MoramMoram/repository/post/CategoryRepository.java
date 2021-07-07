fb71a84ce2aca2e459c8efff44727a698b516939
yujina <jina6962@gmail.com>
2021-07-07 22:24:44 +0900

    [ADD] - 포스트 아이디 찾기

--- src/main/java/radiantMoramMoram/MoramMoram/repository/post/CategoryRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/CategoryRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import radiantMoramMoram.MoramMoram.domain.post.category.Category;
 import radiantMoramMoram.MoramMoram.domain.post.category.CategoryKey;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, CategoryKey> {
+
+    List<Category> findByPostIdOrderById(Integer postId);
 
 }
