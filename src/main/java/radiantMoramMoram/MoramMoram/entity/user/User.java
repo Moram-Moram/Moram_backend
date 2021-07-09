715c190ee23ce564593f5c3c62f669b39a520219
yujina <jina6962@gmail.com>
2021-07-10 00:22:49 +0900

    [ADD] - add user entity

--- src/main/java/radiantMoramMoram/MoramMoram/entity/user/User.java
+++ src/main/java/radiantMoramMoram/MoramMoram/entity/user/User.java
@@ -1,12 +1,13 @@
-package radiantMoramMoram.MoramMoram.domain.user;
+package radiantMoramMoram.MoramMoram.entity.user;
 
-import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+import radiantMoramMoram.MoramMoram.security.auth.Authority;
+import radiantMoramMoram.MoramMoram.security.auth.AuthorityAttributeConverter;
 
 import javax.persistence.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
 @Getter
 @Table(name = "tbl_user")
 @Entity
@@ -14,16 +15,22 @@ public class User {
     @Id
     private String id;
     private String password;
-    private String name;
+    private String nickname;
+    @Column(name = "white_check")
+    private boolean whiteCheck;
+    @Setter
+    @Convert(converter = AuthorityAttributeConverter.class)
+    private Authority role = Authority.ROLE_WATER_DROP;
 
     public User() {
 
     }
 
-    User(String id, String password, String name) {
+    User(String id, String password, String nickname, boolean whiteCheck) {
         this.id = id;
         this.password = password;
-        this.name = name;
+        this.nickname = nickname;
+        this.whiteCheck = whiteCheck;
     }
 
     static public String pwEncrypt(String pw){
