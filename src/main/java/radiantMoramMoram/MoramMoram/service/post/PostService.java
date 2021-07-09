0a6ac4f1b5204577ad6a90b06e7a3a76d00cbe67
yujina <jina6962@gmail.com>
2021-07-10 00:37:04 +0900

    [FIX] - post service

--- src/main/java/radiantMoramMoram/MoramMoram/service/post/PostService.java
+++ src/main/java/radiantMoramMoram/MoramMoram/service/post/PostService.java
@@ -3,7 +3,11 @@ package radiantMoramMoram.MoramMoram.service.post;
 import radiantMoramMoram.MoramMoram.payload.request.post.LikePostRequest;
 import radiantMoramMoram.MoramMoram.payload.request.post.ReportPostRequest;
 import radiantMoramMoram.MoramMoram.payload.request.post.WritePostRequest;
+<<<<<<< Updated upstream
 import radiantMoramMoram.MoramMoram.payload.response.GetPostResponse;
+=======
+import radiantMoramMoram.MoramMoram.payload.response.post.GetPostResponse;
+>>>>>>> Stashed changes
 
 public interface PostService {
 
