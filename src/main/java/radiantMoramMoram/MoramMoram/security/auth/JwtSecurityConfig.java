fadec2db1c2ad54d46906c146cd4e298f6235226
joungeun <lje0818ovo@naver.com>
2021-07-09 16:59:53 +0900

    [ADD] security basics config

---
+++ src/main/java/radiantMoramMoram/MoramMoram/security/auth/JwtSecurityConfig.java
@@ -0,0 +1,36 @@
+package radiantMoramMoram.MoramMoram.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import radiantMoramMoram.MoramMoram.security.token.JwtUtil;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws  Exception{
+        http
+                .cors().and()
+                .csrf().disable()
+
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers("/*").permitAll()
+                .antMatchers("/admin/").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().disable();
+    }
+}
