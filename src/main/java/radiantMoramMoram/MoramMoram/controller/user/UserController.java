3fdc726e9146fa2d495054bb88888e9b36531e4a
joungeun <lje0818ovo@naver.com>
2021-07-09 16:57:07 +0900

    [ADD] user controller add uri

--- src/main/java/radiantMoramMoram/MoramMoram/controller/user/UserController.java
+++ src/main/java/radiantMoramMoram/MoramMoram/controller/user/UserController.java
@@ -1,28 +1,27 @@
 package radiantMoramMoram.MoramMoram.controller.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import radiantMoramMoram.MoramMoram.payload.request.user.LoginRequest;
 import radiantMoramMoram.MoramMoram.payload.request.user.SignUpRequest;
-import radiantMoramMoram.MoramMoram.security.token.TokenDTO;
+import radiantMoramMoram.MoramMoram.payload.response.token.TokenResponse;
 import radiantMoramMoram.MoramMoram.service.user.UserServiceImpl;
-import radiantMoramMoram.MoramMoram.entity.user.User;
-import radiantMoramMoram.MoramMoram.security.token.JwtUtil;
 
 @RequiredArgsConstructor
 @RestController
 public class UserController {
 
     private final UserServiceImpl userService;
-    private final JwtUtil jwtUtil;
 
     @PostMapping("/user")
     public void join(@RequestBody SignUpRequest user){
         userService.join(user);
+    }
+
+    @PostMapping("/auth")
+    public TokenResponse login(@RequestBody LoginRequest fUser){
+        return userService.login(fUser);
     }
 }
