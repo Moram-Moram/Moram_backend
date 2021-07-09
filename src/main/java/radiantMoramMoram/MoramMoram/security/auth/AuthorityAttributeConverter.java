577ecd02fedd55a725804737eac9e0d49075e18d
joungeun <lje0818ovo@naver.com>
2021-07-09 16:44:35 +0900

    [ADD] add rank enum type and converter

---
+++ src/main/java/radiantMoramMoram/MoramMoram/security/auth/AuthorityAttributeConverter.java
@@ -0,0 +1,23 @@
+package radiantMoramMoram.MoramMoram.security.auth;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.stream.Stream;
+
+@Converter(autoApply = true)
+public class AuthorityAttributeConverter implements AttributeConverter<Authority, Integer> {
+
+    @Override
+    public Integer convertToDatabaseColumn(Authority a) {
+        return a.num;
+    }
+
+    @Override
+    public Authority convertToEntityAttribute(Integer num) {
+
+        return Stream.of(Authority.values())
+                .filter(c -> c.num == num)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
