367102240485e55abfe030cf5427a628a5a634ff
yujina <jina6962@gmail.com>
2021-07-10 00:26:58 +0900

    [ADD] - add error response

---
+++ src/main/java/radiantMoramMoram/MoramMoram/exception/ErrorResponse.java
@@ -0,0 +1,14 @@
+package radiantMoramMoram.MoramMoram.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private final int status;
+
+    private final String message;
+
+}
