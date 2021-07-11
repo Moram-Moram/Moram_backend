000139e774f587c078d031c91463f3a6157b75c3
joungeun <lje0818ovo@naver.com>
2021-07-11 13:10:51 +0900

    [TEST] Comment insert test

--- src/main/java/radiantMoramMoram/MoramMoram/payload/request/comment/WriteCommentRequest.java
+++ src/main/java/radiantMoramMoram/MoramMoram/payload/request/comment/WriteCommentRequest.java
@@ -1,14 +1,9 @@
 package radiantMoramMoram.MoramMoram.payload.request.comment;
 
+import lombok.Builder;
 import lombok.Getter;
-import radiantMoramMoram.MoramMoram.entity.post.Post;
-import radiantMoramMoram.MoramMoram.entity.user.User;
 
-import javax.persistence.Column;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
+@Builder
 @Getter
 public class WriteCommentRequest {
     private String content;
