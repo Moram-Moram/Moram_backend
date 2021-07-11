1887d6794fc4927b170fc5a8ab3ffb60fb98fced
yujina <jina6962@gmail.com>
2021-07-11 22:07:12 +0900

    [FIX] - change package

--- src/main/java/radiantMoramMoram/MoramMoram/repository/post/LikePostRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/LikePostRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import radiantMoramMoram.MoramMoram.entity.post.Post;
 import radiantMoramMoram.MoramMoram.entity.post.like.LikePost;
 import radiantMoramMoram.MoramMoram.entity.post.like.LikePostKey;
-import radiantMoramMoram.MoramMoram.domain.user.User;
+import radiantMoramMoram.MoramMoram.entity.user.User;
 
 public interface LikePostRepository extends JpaRepository<LikePost, LikePostKey> {
 
