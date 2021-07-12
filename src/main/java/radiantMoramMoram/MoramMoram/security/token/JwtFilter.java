dcf54465b1982631e86ba3105187d42f6a49a886
joungeun <lje0818ovo@naver.com>
2021-07-12 22:20:31 +0900

    [ADD] Create JwtFilter

--- src/main/java/radiantMoramMoram/MoramMoram/security/token/JwtFilter.java
+++ src/main/java/radiantMoramMoram/MoramMoram/security/token/JwtFilter.java
@@ -1,32 +1,38 @@
 package radiantMoramMoram.MoramMoram.security.token;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import radiantMoramMoram.MoramMoram.service.user.CustomUserDetailService;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+@Component
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
-    public static final String AUTHORIZATION_HEADER = "";
+    private final CustomUserDetailService userDetailsService;
+    public static final String AUTHORIZATION_HEADER = "Authorization";
     private final JwtUtil jwtUtil;
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        String token = request.getHeader(AUTHORIZATION_HEADER);
+        String accessToken = request.getHeader(AUTHORIZATION_HEADER);
 
-        if (StringUtils.hasText(token) && jwtUtil.validateToken(token)){
-            // Authentication authentication = jwtUtil.getAuthentication(token);
-            // SecurityContextHolder.getContext().setAuthentication(authentication); 권한
+        if (StringUtils.hasText(accessToken) && jwtUtil.validateToken(accessToken)){
+            String userId = jwtUtil.getUserIdFromJwtToken(accessToken);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
+
+
+            System.out.println(userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities()));
         }
         filterChain.doFilter(request, response);
-
     }
 }
