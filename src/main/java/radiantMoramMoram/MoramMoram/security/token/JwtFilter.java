7dbf1cb9b417ddb06202e5c64396f5b3c1ffdcc1
joungeun <lje0818ovo@naver.com>
2021-07-13 19:05:13 +0900

    [FIX] Remove CustomUserDetailService dependencies

--- src/main/java/radiantMoramMoram/MoramMoram/security/token/JwtFilter.java
+++ src/main/java/radiantMoramMoram/MoramMoram/security/token/JwtFilter.java
@@ -1,37 +1,37 @@
 package radiantMoramMoram.MoramMoram.security.token;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-import radiantMoramMoram.MoramMoram.service.user.CustomUserDetailService;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-@Component
+
+@Log
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
-    private final CustomUserDetailService userDetailsService;
     public static final String AUTHORIZATION_HEADER = "Authorization";
     private final JwtUtil jwtUtil;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+        log.info("filter 진입");
         String accessToken = request.getHeader(AUTHORIZATION_HEADER);
 
         if (StringUtils.hasText(accessToken) && jwtUtil.validateToken(accessToken)){
             String userId = jwtUtil.getUserIdFromJwtToken(accessToken);
-            UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
+            UserDetails userDetails = jwtUtil.userAuthReturn(userId);
 
 
             System.out.println(userDetails.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities()));
+            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(userDetails,null, userDetails.getAuthorities()));
         }
         filterChain.doFilter(request, response);
     }
