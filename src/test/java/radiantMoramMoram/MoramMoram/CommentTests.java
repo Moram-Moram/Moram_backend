c7a2ebcca2bea4d198df90da4b77d6822361ee49
joungeun <lje0818ovo@naver.com>
2021-07-13 23:03:16 +0900

    [TEST] Comment insert test

--- src/test/java/radiantMoramMoram/MoramMoram/CommentTests.java
+++ src/test/java/radiantMoramMoram/MoramMoram/CommentTests.java
@@ -3,38 +3,23 @@ package radiantMoramMoram.MoramMoram;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import radiantMoramMoram.MoramMoram.entity.post.Post;
-import radiantMoramMoram.MoramMoram.entity.user.User;
 import radiantMoramMoram.MoramMoram.payload.request.comment.WriteCommentRequest;
-import radiantMoramMoram.MoramMoram.repository.UserRepository;
-import radiantMoramMoram.MoramMoram.repository.post.PostRepository;
 import radiantMoramMoram.MoramMoram.service.comment.CommentService;
-import radiantMoramMoram.MoramMoram.service.post.PostServiceImpl;
 
-import java.util.Optional;
 
 @SpringBootTest
 public class CommentTests {
     @Autowired
     CommentService commentService;
 
-    @Autowired
-    PostRepository postRepository;
-
-    @Autowired
-    UserRepository userRepository;
-
     @Test
     public void commentInsertTest(){
-        WriteCommentRequest req = WriteCommentRequest.builder()
-                .content("진짜 짱이다")
+        String content = "content";
+        String userId = "00000";
+        int postId =2;
+        WriteCommentRequest commentReq = WriteCommentRequest.builder()
+                .content(content)
                 .build();
-        Optional<Post> post = postRepository.findById(2);
-        Optional<User> user = userRepository.findById("00000");
-
-        if(post.isPresent() && user.isPresent()){
-            commentService.writeComment(req, post.get(), user.get());
-        }
-
+        commentService.writeComment(commentReq, userId, postId);
     }
 }
