d434b04db4c404601baa15f19e30952b7c66fab7
joungeun <lje0818ovo@naver.com>
2021-07-14 20:07:47 +0900

    chore

---
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/PostRepository.java
@@ -0,0 +1,7 @@
+package radiantMoramMoram.MoramMoram.repository.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import radiantMoramMoram.MoramMoram.entity.post.Post;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+}
