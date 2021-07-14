be13a4d5689e4c5db3a277fffff0fe54564e401a
yujina <jina6962@gmail.com>
2021-07-14 19:47:31 +0900

    [ADD] - @IdClass

--- src/main/java/radiantMoramMoram/MoramMoram/entity/post/like/LikePost.java
+++ src/main/java/radiantMoramMoram/MoramMoram/entity/post/like/LikePost.java
@@ -9,9 +9,11 @@ import radiantMoramMoram.MoramMoram.entity.user.User;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.OneToOne;
 
 @Entity(name = "tbl_like")
+@IdClass(LikePostKey.class)
 @Getter
 @Builder
 @NoArgsConstructor
