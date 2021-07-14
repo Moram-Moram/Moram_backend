16f3ba082a435d668f37c8b6b2b16437a56ee21d
joungeun <lje0818ovo@naver.com>
2021-07-14 19:24:12 +0900

    Package change

--- src/main/java/radiantMoramMoram/MoramMoram/security/token/JwtFilter.java
+++ src/main/java/radiantMoramMoram/MoramMoram/security/token/JwtFilter.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-import radiantMoramMoram.MoramMoram.exception.TokenException;
+import radiantMoramMoram.MoramMoram.error.TokenException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
