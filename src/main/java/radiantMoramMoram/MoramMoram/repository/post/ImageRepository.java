e92970bc67bd1cbcf28ef7fc13a52be8cc847123
yujina <jina6962@gmail.com>
2021-07-15 19:37:56 +0900

    [ADD] - import

--- src/main/java/radiantMoramMoram/MoramMoram/repository/post/ImageRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/ImageRepository.java
@@ -3,6 +3,7 @@ package radiantMoramMoram.MoramMoram.repository.post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import radiantMoramMoram.MoramMoram.entity.post.image.Image;
+import radiantMoramMoram.MoramMoram.entity.post.image.ImageKey;
 
 import java.util.List;
 
