5771d7179f063965381a9a2bb25f7d613780a9af
yujina <jina6962@gmail.com>
2021-07-15 17:01:47 +0900

    [DELETE] - 레포 내용

--- src/main/java/radiantMoramMoram/MoramMoram/repository/post/CategoryRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/CategoryRepository.java
@@ -4,10 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import radiantMoramMoram.MoramMoram.entity.post.category.Category;
 import radiantMoramMoram.MoramMoram.entity.post.category.CategoryKey;
 
-import java.util.List;
-
 public interface CategoryRepository extends JpaRepository<Category, CategoryKey> {
-
-    List<Category> findByPostIdOrderById(Integer postId);
-
 }
