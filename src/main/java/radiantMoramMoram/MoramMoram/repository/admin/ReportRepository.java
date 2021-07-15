9a1d7d833d048348be003d253e2c811f8ad3705a
임서영 <sylim0318@naver.com>
2021-07-15 11:15:13 +0900

    [FIX] 레포지토리 어노테이션 추가

--- src/main/java/radiantMoramMoram/MoramMoram/repository/admin/ReportRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/admin/ReportRepository.java
@@ -4,8 +4,10 @@ package radiantMoramMoram.MoramMoram.repository.admin;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import radiantMoramMoram.MoramMoram.entity.post.Post;
 
+@Repository
 public interface ReportRepository extends CrudRepository<Post, Integer> {
 
     Page<Post> findAllBy(Pageable pageable);
