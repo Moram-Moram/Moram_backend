c3d55fe23957e007bfdcc6c145695e8edd3c7425
임서영 <sylim0318@naver.com>
2021-07-20 03:26:39 +0900

    [ADD] 유저, 토큰 에러 추가

--- src/main/java/radiantMoramMoram/MoramMoram/exception/ErrorCode.java
+++ src/main/java/radiantMoramMoram/MoramMoram/exception/ErrorCode.java
@@ -9,6 +9,8 @@ public enum ErrorCode {
 
     BAD_REQUEST(400, "Bad Request"),
     INVALID_TOKEN(401,"Invalid Token"),
+    EXPIRED_TOKEN(401,"Expired Token"),
+    AUTHORIZATION_FAILURE(403,"Authorization Failure"),
     USER_NOT_FOUND(404,"User Not Found"),
     POST_NOT_FOUND(404, "Post Not Found"),
     IMAGE_NOT_FOUND(404, "image not found");
