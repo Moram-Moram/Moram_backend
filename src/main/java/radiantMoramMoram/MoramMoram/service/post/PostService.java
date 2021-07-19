d473c3924268e1d73bd331d07d1db02dad12f0db
yujina <jina6962@gmail.com>
2021-07-19 21:17:14 +0900

    [ADD] - getPost에 token 변수 추가

--- src/main/java/radiantMoramMoram/MoramMoram/service/post/PostService.java
+++ src/main/java/radiantMoramMoram/MoramMoram/service/post/PostService.java
@@ -3,13 +3,13 @@ package radiantMoramMoram.MoramMoram.service.post;
 import radiantMoramMoram.MoramMoram.payload.request.post.LikePostRequest;
 import radiantMoramMoram.MoramMoram.payload.request.post.ReportPostRequest;
 import radiantMoramMoram.MoramMoram.payload.request.post.WritePostRequest;
-import radiantMoramMoram.MoramMoram.payload.response.GetPostResponse;
+import radiantMoramMoram.MoramMoram.payload.response.post.GetPostResponse;
 
 public interface PostService {
 
     void writePost(WritePostRequest writePostRequest, String token);
 
-    GetPostResponse getPost(Integer postId);
+    GetPostResponse getPost(Integer postId, String token);
 
     void deletePost(Integer postId);
 
