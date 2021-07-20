e8c0be62e87dc46c9865945487f6311bf95a81d7
joungeun <lje0818ovo@naver.com>
2021-07-21 01:32:47 +0900

    [ADD] Add query

--- src/main/java/radiantMoramMoram/MoramMoram/repository/post/ImageRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/ImageRepository.java
@@ -1,6 +1,7 @@
 package radiantMoramMoram.MoramMoram.repository.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import radiantMoramMoram.MoramMoram.entity.post.Post;
 import radiantMoramMoram.MoramMoram.entity.post.image.Image;
 
 import java.util.List;
@@ -8,5 +9,7 @@ import java.util.List;
 public interface ImageRepository extends JpaRepository<Image, Integer> {
 
     List<Image> findByPostOrderById(Integer postId);
+
+    List<Image> findByPostOrderById(Post post);
 
 }
