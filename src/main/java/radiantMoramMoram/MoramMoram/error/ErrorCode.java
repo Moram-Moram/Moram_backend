b1393fd330be9e40452df2a57f1abc0a7d8891ad
yujina <jina6962@gmail.com>
2021-07-22 22:04:25 +0900

    [ADD] - post doesn't exist

--- src/main/java/radiantMoramMoram/MoramMoram/error/ErrorCode.java
+++ src/main/java/radiantMoramMoram/MoramMoram/error/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
     BAD_REQUEST(400, "Bad Request"),
     INVALID_TOKEN(401,"Invalid Token"),
     EXPIRED_TOKEN(401,"Expired Token"),
+    POST_DOES_NOT_EXIST(404, "Post doesn't exist"),
     USER_NOT_FOUND(404,"User Not Found"),
     POST_NOT_FOUND(404, "Post Not Found"),
     POST_DOES_NOT_EXIST(404, "Post doesn't exist"),
