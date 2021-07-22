354835368c75eb442405de9cbf1af507fcc66c87
yujina <jina6962@gmail.com>
2021-07-22 21:44:18 +0900

    [DELETE] - builder

--- src/main/java/radiantMoramMoram/MoramMoram/entity/post/like/LikePostKey.java
+++ src/main/java/radiantMoramMoram/MoramMoram/entity/post/like/LikePostKey.java
@@ -1,10 +1,7 @@
 package radiantMoramMoram.MoramMoram.entity.post.like;
 
-import lombok.Builder;
-
 import java.io.Serializable;
 
-@Builder
 public class LikePostKey implements Serializable {
 
     private int post;
