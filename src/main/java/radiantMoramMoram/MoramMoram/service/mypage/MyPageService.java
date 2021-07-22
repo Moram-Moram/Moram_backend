2a23e56c5572083ab225529325b66376f71d57ea
임서영 <sylim0318@naver.com>
2021-07-22 15:34:13 +0900

    [DELETE] 유저 서비스로 이동

--- src/main/java/radiantMoramMoram/MoramMoram/service/mypage/MyPageService.java
+++
@@ -1,9 +0,0 @@
-package radiantMoramMoram.MoramMoram.service.mypage;
-
-import radiantMoramMoram.MoramMoram.payload.response.mypage.MyPageResponse;
-
-public interface MyPageService {
-
-    MyPageResponse getMyPage(String userId);
-
-}
