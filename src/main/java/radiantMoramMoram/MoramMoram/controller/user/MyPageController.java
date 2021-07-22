a97e36d4f34b0ebcc72f045bcc04907ac3d5fa21
임서영 <sylim0318@naver.com>
2021-07-22 23:47:32 +0900

    [ADD] 회원 삭제 & 수정 추가

--- src/main/java/radiantMoramMoram/MoramMoram/controller/user/MyPageController.java
+++ src/main/java/radiantMoramMoram/MoramMoram/controller/user/MyPageController.java
@@ -3,8 +3,12 @@ package radiantMoramMoram.MoramMoram.controller.user;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.Parameter;
 import org.springframework.web.bind.annotation.*;
+import radiantMoramMoram.MoramMoram.entity.user.User;
+import radiantMoramMoram.MoramMoram.payload.response.mypage.MyPagePostResponse;
 import radiantMoramMoram.MoramMoram.payload.response.mypage.MyPageResponse;
 import radiantMoramMoram.MoramMoram.service.user.UserService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,9 +17,19 @@ public class MyPageController {
 
     private final UserService userService;
 
-    //@RequestMapping(value = "{userId}",  method=RequestMethod.GET)
-    @GetMapping("{userId}")
+    @GetMapping("/{userId}")
     public MyPageResponse getMyPage(@PathVariable String userId) {
         return userService.getMyPage(userId);
     }
+
+    @GetMapping("/{userId}/mine")
+    public List<MyPagePostResponse> getMyPost(@PathVariable User user, int postId) {
+        return userService.getMyPagePost(user, postId);
+    }
+    
+    @GetMapping("/{userId}/like")
+    public List<MyPagePostResponse> getLikePost(@PathVariable User userId, int postId) {
+        return userService.getLikePost(userId, postId);
+    }
+
 }
