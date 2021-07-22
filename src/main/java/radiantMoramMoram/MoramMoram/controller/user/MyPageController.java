ce37d5d27104bcf2b2ba17687d7cbc9c0a48252d
임서영 <sylim0318@naver.com>
2021-07-22 15:35:13 +0900

    [FIX] 서비스 변경

--- src/main/java/radiantMoramMoram/MoramMoram/controller/user/MyPageController.java
+++ src/main/java/radiantMoramMoram/MoramMoram/controller/user/MyPageController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import radiantMoramMoram.MoramMoram.payload.response.mypage.MyPageResponse;
-import radiantMoramMoram.MoramMoram.service.mypage.MyPageService;
+import radiantMoramMoram.MoramMoram.service.user.UserService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user/{userId}")
 public class MyPageController {
 
-    private MyPageService myPageService;
+    private UserService userService;
 
     @GetMapping
     public MyPageResponse getMyPage(@PathVariable String userId) {
-        return myPageService.getMyPage(userId);
+        return userService.getMyPage(userId);
     }
 }
