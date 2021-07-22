4dab6a80e7762fd0bde6d7b2d3390342f1cc7b05
임서영 <sylim0318@naver.com>
2021-07-23 07:12:01 +0900

    [ADD] findByUser 추가

--- src/main/java/radiantMoramMoram/MoramMoram/repository/post/PostRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/PostRepository.java
@@ -1,13 +1,21 @@
 package radiantMoramMoram.MoramMoram.repository.post;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import radiantMoramMoram.MoramMoram.entity.post.Post;
+import radiantMoramMoram.MoramMoram.entity.user.User;
+import radiantMoramMoram.MoramMoram.payload.response.mypage.MyPagePostResponse;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
     @Query(value = "select * from moram_moram.tbl_post order by rand() limit 1",nativeQuery = true)
     Post findRandomPost();
+
+    List<Post> findByUser(User user);
 
 }
