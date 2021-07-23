e4485889034f00c4e1145fe7cd0c8ddf9386ccd5
yujina <jina6962@gmail.com>
2021-07-23 17:44:43 +0900

    [ADD] - 중복검사

--- src/main/java/radiantMoramMoram/MoramMoram/service/user/UserService.java
+++ src/main/java/radiantMoramMoram/MoramMoram/service/user/UserService.java
@@ -19,6 +19,8 @@ public interface UserService {
 
     MyPageResponse getMyPage(String userId);
 
-    void duplicateCheck(SignUpRequest signUpRequest);
+    void duplicateIdCheck(String userId);
+
+    void duplicateNickNameCheck(String userNickName);
 
 }
