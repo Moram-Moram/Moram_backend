e19f44928cc2a60875a301494e890bf729576ee9
임서영 <sylim0318@naver.com>
2021-07-23 20:41:06 +0900

    [FIX] 매개변수 변경

--- src/main/java/radiantMoramMoram/MoramMoram/repository/post/ImageRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/ImageRepository.java
@@ -1,12 +1,13 @@
 package radiantMoramMoram.MoramMoram.repository.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import radiantMoramMoram.MoramMoram.entity.post.Post;
 import radiantMoramMoram.MoramMoram.entity.post.image.Image;
 
 import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
 
-    List<Image> findByPostOrderById(Integer postId);
+    List<Image> findByPostOrderById(Post post);
 
 }
