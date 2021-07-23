7acdbb4ea97eefc00489828fb2b98f991fa43504
yujina <jina6962@gmail.com>
2021-07-23 23:30:13 +0900

    [ADD] - find by postId, delete by postId

--- src/main/java/radiantMoramMoram/MoramMoram/repository/CommentRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/CommentRepository.java
@@ -1,10 +1,16 @@
 package radiantMoramMoram.MoramMoram.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import radiantMoramMoram.MoramMoram.entity.post.Post;
 import radiantMoramMoram.MoramMoram.entity.post.comment.Comment;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends CrudRepository<Comment, Integer> {
     List<Comment> findByPost_Id(int postId);
+
+    Optional<Post> findByPostId(Integer postId);
+
+    void deleteByPostId(Integer postId);
 }
--- src/main/java/radiantMoramMoram/MoramMoram/repository/post/ImageRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/ImageRepository.java
@@ -6,9 +6,14 @@ import radiantMoramMoram.MoramMoram.entity.post.image.Image;
 import radiantMoramMoram.MoramMoram.entity.post.image.ImageKey;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ImageRepository extends JpaRepository<Image, ImageKey> {
 
     List<Image> findByPostOrderById(Post post);
+
+    Optional<Post> findByPostId(Integer postId);
+
+    void deleteByPostId(Integer postId);
 
 }
