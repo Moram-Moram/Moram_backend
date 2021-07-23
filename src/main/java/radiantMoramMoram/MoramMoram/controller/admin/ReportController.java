7ac905a8da86d048ba4132074826449eababfb1b
임서영 <sylim0318@naver.com>
2021-07-23 12:10:47 +0900

    [FIX] 매개변수 변경

--- src/main/java/radiantMoramMoram/MoramMoram/controller/admin/ReportController.java
+++ src/main/java/radiantMoramMoram/MoramMoram/controller/admin/ReportController.java
@@ -3,9 +3,10 @@ package radiantMoramMoram.MoramMoram.controller.admin;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-import radiantMoramMoram.MoramMoram.payload.response.admin.ReportListResponse;
 import radiantMoramMoram.MoramMoram.payload.response.admin.ReportPostResponse;
 import radiantMoramMoram.MoramMoram.service.admin.ReportService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +16,8 @@ public class ReportController {
     private final ReportService reportService;
 
     @GetMapping
-    public ReportListResponse getReportList(@PathVariable Pageable pageable) {
-        return reportService.getReportList(pageable);
+    public List<ReportPostResponse> getReportList() {
+        return reportService.getReportList();
     }
 
     @GetMapping("/postId")
