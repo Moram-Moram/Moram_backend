a0a0484ae7fa173e826ec6722431aeb3cb343f53
yujina <jina6962@gmail.com>
2021-07-23 17:43:37 +0900

    [FIX] - 타입 변경

--- src/main/java/radiantMoramMoram/MoramMoram/repository/post/ImageRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/ImageRepository.java
@@ -3,15 +3,14 @@ package radiantMoramMoram.MoramMoram.repository.post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import radiantMoramMoram.MoramMoram.entity.post.Post;
 import radiantMoramMoram.MoramMoram.entity.post.image.Image;
+import radiantMoramMoram.MoramMoram.entity.post.image.ImageKey;
 
 import java.util.List;
 
-public interface ImageRepository extends JpaRepository<Image, Integer> {
+public interface ImageRepository extends JpaRepository<Image, ImageKey> {
 
     List<Image> findByPostOrderById(Integer postId);
 
     List<Image> findByPostOrderById(Post post);
-
-    List<Image> findAllByPost(Post post);
 
 }
