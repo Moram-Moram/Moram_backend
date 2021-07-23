ab48beb4e94e60054e594339f4e4e243df4d6632
임서영 <sylim0318@naver.com>
2021-07-23 20:44:44 +0900

    [FIX] 어노테이션 추가

--- src/main/java/radiantMoramMoram/MoramMoram/repository/post/PostRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/PostRepository.java
@@ -1,11 +1,13 @@
 package radiantMoramMoram.MoramMoram.repository.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import radiantMoramMoram.MoramMoram.entity.post.Post;
 import radiantMoramMoram.MoramMoram.payload.response.admin.ReportPostResponse;
 
 import java.util.List;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
     List<Post> findByReport(boolean report);
