b7c959cc8cf4c062c57749d197d7683b76402a7b
yujina <jina6962@gmail.com>
2021-07-23 17:39:23 +0900

    [DELETE] - builder 삭제

--- src/main/java/radiantMoramMoram/MoramMoram/entity/post/image/ImageKey.java
+++ src/main/java/radiantMoramMoram/MoramMoram/entity/post/image/ImageKey.java
@@ -1,10 +1,7 @@
 package radiantMoramMoram.MoramMoram.entity.post.image;
 
-import lombok.Builder;
-
 import java.io.Serializable;
 
-@Builder
 public class ImageKey implements Serializable {
 
     private int post;
