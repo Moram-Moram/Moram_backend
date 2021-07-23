08a7aba55377a0baca0d77dd792923827ac30c22
yujina <jina6962@gmail.com>
2021-07-23 22:43:37 +0900

    [FIX] - class name change

---
+++ src/main/java/radiantMoramMoram/MoramMoram/payload/response/post/PostResponse.java
@@ -0,0 +1,33 @@
+package radiantMoramMoram.MoramMoram.payload.response.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostResponse {
+
+    private Integer postId;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    private String user;
+
+    private LocalDate date;
+
+    private List<String> fileName;
+
+    private Integer likeNum;
+
+}
