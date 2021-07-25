10d4ed9e5543a10f3f4516d950afc2961b5e0d03
yujina <jina6962@gmail.com>
2021-07-25 20:47:04 +0900

    [FIX]

--- src/main/java/radiantMoramMoram/MoramMoram/repository/post/LikePostRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/LikePostRepository.java
@@ -19,6 +19,7 @@ public interface LikePostRepository extends JpaRepository<LikePost, LikePostKey>
 
     @Query(value = "select count(*) from moram_moram.tbl_like where post_id = :postId", nativeQuery = true)
     int postLikeNum(int postId);
+
     List<LikePost> findByUser(User user);
 
 }
