ec45a97ce0c5fb92cc14201986b6d71f338cbcaa
joungeun <lje0818ovo@naver.com>
2021-07-27 17:13:32 +0900

    [ADD] Delete query add @Transactional

--- src/main/java/radiantMoramMoram/MoramMoram/repository/post/ImageRepository.java
+++ src/main/java/radiantMoramMoram/MoramMoram/repository/post/ImageRepository.java
@@ -5,11 +5,13 @@ import radiantMoramMoram.MoramMoram.entity.post.Post;
 import radiantMoramMoram.MoramMoram.entity.post.image.Image;
 import radiantMoramMoram.MoramMoram.entity.post.image.ImageKey;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, ImageKey> {
 
     List<Image> findByPostOrderById(Post post);
+    @Transactional
     void deleteByPostId(int postId);
 
 }
